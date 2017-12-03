f0449b59e102d0562a3c840ebb9e9830fee4879e
Julio <juliodevasquez@gmail.com>
2017-12-03 13:34:59 -0600

    implements Post creation + show + bbd tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,7 +11,24 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
+
+
+  describe 'creation' do 
+      it "has a new form that can be reached" do 
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+      end
+
+      it "can be created from new form page" do 
+      visit new_post_path
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'Some Rationale'
+      click_on "Save"
+
+      expect(page).to have_content("Some Rationale")
+    end    
   end
+ end
 end
 
 
