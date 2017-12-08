39fa0bc3f70160a325a4cda6913f16abf02640af
Julio <juliodevasquez@gmail.com>
2017-12-07 19:42:46 -0600

    refactores rspec + post rending

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create(first_name: "Julio", last_name: "Vasquez" , email: "test@test.com", password: "12345678", password_confirmation: "12345678")
+
+puts "1 User Created"
+
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content")
+	Post.create!(date: Date.today, rationale: "#{post} rationale content" , user_id: @user.id)
 end
 
 puts "100 posts have been created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,8 +6,11 @@ describe 'navigate' do
       login_as(user, :scope => :user)
   end
   describe 'index' do
-    it 'can be reached successfully' do
+    before do 
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
