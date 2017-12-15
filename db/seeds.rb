c43c54d628a45a294eb5ef8d368f10dc9a52bfe0
Julio <juliodevasquez@gmail.com>
2017-12-14 18:58:44 -0600

    customized admin dashboard to include enum

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 @user = User.create(first_name: "Julio", last_name: "Vasquez" , email: "test@test.com", password: "12345678", password_confirmation: "12345678")
+@admin_user = AdminUser.create(first_name: "Julio", last_name: "Vasquez" , email: "admin@admin.com", password: "12345678", password_confirmation: "12345678")
 
-puts "1 User Created"
+puts "1 User Created + 1 adminUser"
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content" , user_id: @user.id)
