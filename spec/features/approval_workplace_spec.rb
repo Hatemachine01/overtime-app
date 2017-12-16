eff7042a526555c8a8949fe73837e3a3a0542200
Julio <juliodevasquez@gmail.com>
2017-12-16 09:54:39 -0600

    hides approval form from non admin users

--- spec/features/approval_workplace_spec.rb
+++ spec/features/approval_workplace_spec.rb
@@ -3,20 +3,31 @@ require 'rails_helper'
 describe 'navigate' do 
 	before do 
 	  @admin_user = FactoryGirl.create(:admin_user)
-	login_as(@admin_user, :scope => :user)
+	  login_as(@admin_user, :scope => :user)
    end
 
 describe 'edit' do 
 	before do 
 		@post = FactoryGirl.create(:post)
-	end
-
-	it 'it has a status that can be edited on the form' do 
 		visit edit_post_path(@post)
 
-		choose ('radio_button')
+	end
+
+	it 'it has a status that can be edited on the form by an admin' do 
+	
+		choose('radio_button')
 		click_on "Save"
 		expect(@post.reload.status).to eq('approved')
+	end
+
+
+	it 'cannot be edited by an admin' do
+		logout(:user)
+		user = FactoryGirl.create(:user)
+      	login_as(user, :scope => :user)
+
+      	visit edit_post_path(@post)
+      	expect(page).to_not have_content("Approved")
 	end
   end
 end
